fbe9b0196b75ba381f26b4500084c457a58eceb9
chaithanya <chaithanya1812@gmail.com>
2025-04-17 19:42:32 +0530

    ss

--- main.tf
+++ main.tf
@@ -1,19 +1,16 @@
-# resource "aws_instance" "main" {
-#
-#   for_each = toset(var.instance_types)
-#   ami = "ami-00a929b66ed6e0de6"
-#   instance_type = "${each.value}"
-#
-#   tags  = {
-#     Name = "instance-${each.key}"
-#   }
-# }
-
-variable "instance_types" {
-  type = list(string)
-  default = ["t2.micro","t3.small","t3.medium"]
+variable "one" {
+  default = ["one","two"]
 }
 
-output "instances" {
-  value = {for i in var.instance_types: "instance_type" => i }
+output "all" {
+  value =<<EOT
+%{~for i in var.one ~}
+${i}
+%{~ endfor ~}
+EOT
 }
+
+
+
+
+
