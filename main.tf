3e63c4b42706eedacfa2ed0334daa18a173becd6
chaithanya <chaithanya1812@gmail.com>
2025-04-22 13:00:04 +0530

    ss

--- main.tf
+++ main.tf
@@ -1,7 +1,1 @@
-variable "one" {
-  default = ["one","two",["three","four"],"six"]
-}
-
-output "all" {
-  value = flatten(var.one)
-}
+ ##
--- vars.tfvars
+++
@@ -1,6 +0,0 @@
-names = {
-  name = "one"
-  name1 = "two"
-  all   = [1,2]
-}
-
